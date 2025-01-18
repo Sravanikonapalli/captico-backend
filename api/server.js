1f6e49ef281a0a31a1ee0cbbb433b7d532dde8ba
Sravanikonapalli <konapallisravani@gmail.com>
2025-01-18 08:38:27 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: 'https://captico-frontend.vercel.app/api', 
+  origin: 'https://captico-frontend.vercel.app', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
