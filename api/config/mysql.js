ba7a7f630e56b125d42bff60352f7c76f431612b
Sravanikonapalli <konapallisravani@gmail.com>
2025-01-18 08:46:44 +0530

    Update mysql.js

--- api/config/mysql.js
+++ api/config/mysql.js
@@ -7,6 +7,19 @@ const pool = mysql.createPool({
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10, // Adjust based on traffic
+  queueLimit: 0,
 });
+
+pool.getConnection((err) => {
+  if (err) {
+    console.error('MySQL Connection Failed:', err.message);
+    process.exit(1);
+  } else {
+    console.log('MySQL Connected');
+  }
+});
+
 
 module.exports = pool.promise();
